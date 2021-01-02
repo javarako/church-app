21e943db24fe9d584b2d77da99473a9425dc1412
Ko Brad <javarako@gmail.com>
2021-01-02 15:41:59 -0500

    add budget mgt

--- src/main/java/com/javarako/akuc/controller/BudgetController.java
+++ src/main/java/com/javarako/akuc/controller/BudgetController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -64,6 +65,7 @@ public class BudgetController {
 		}
 	}
 
+	@Transactional
 	@PostMapping("/budgets/{year}")
 	public List<Budget> upload(@PathVariable("year") int year, @RequestBody List<Budget> budgets) {
 		try {
